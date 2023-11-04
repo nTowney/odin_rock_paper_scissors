f412a2a77fcc28502383f4d0b21588300ad0cbb1
nTowney <144981300+nTowney@users.noreply.github.com>
2023-11-03 21:19:55 -0700

    Pseudocode created in .js file
    I'm going to begin breaking it down step by step;
    I'm going to try and get it to run 5 total times first, then I will try
    and create it as a loop that counts

---
+++ index.js
@@ -0,0 +1,24 @@
+// Have the computer pick rock, paper, or scissors at random;
+// Store that result in a variable called compChoice;
+// Ask the user to choose rock, paper, or scissors;
+//     This needs to NOT be case sensitive;
+// Store that result in a variable called playerChoice;
+// Compare the computer's choice to the user's choice;
+//     If the computer chose "Rock", check:
+//         If the player chose "Rock", log "It's a tie!";
+//         If the player chose "Paper", log "You win!";
+//         If the player chose "Scissors", log "The computer wins!";
+//     If the computer chose "Paper", check:
+//         If the player chose "Rock", log "The computer wins!";
+//         If the player chose "Paper", log "It's a tie!";
+//         If the player chose "Scissors", log "You win!";
+//     If the computer chose "Scissors", check:
+//         If the player chose "Rock", log "You win!";
+//         If the player chose "Paper", log "The computer wins!";
+//         If the player chose "Scissors", log "It's a tie!";
+// If the player wins, add a point to the player count;
+// If the computer wins, add a point to the computer count;
+// If the result is a tie, add a point to the tie count;
+//     The game is over when a total of 5 wins are accumulated;
+// After the game ends, declare a total winner;
+//     Provide stats (i.e., computer won x, player won y);

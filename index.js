81183a947141b7e58d4869f41743d6b8a7e82fdb
nTowney <144981300+nTowney@users.noreply.github.com>
2023-11-04 22:50:37 -0700

    Roughed out a plan for creating a counter for scores

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+function playRound(){
 // Have the computer pick rock, paper, or scissors at random;
 // Store that result in a variable called compChoice;
 function getCompChoice(){
@@ -20,6 +21,18 @@ let playerChoice = getPlayerChoice();
 console.log("Computer chooses " + compChoice + ", player chooses " + playerChoice);
 
 // Compare the computer's choice to the user's choice;
+//     If the computer chose "Rock", check:
+//         If the player chose "Rock", log "It's a tie!";
+//         If the player chose "Paper", log "You win!";
+//         If the player chose "Scissors", log "The computer wins!";
+//     If the computer chose "Paper", check:
+//         If the player chose "Rock", log "The computer wins!";
+//         If the player chose "Paper", log "It's a tie!";
+//         If the player chose "Scissors", log "You win!";
+//     If the computer chose "Scissors", check:
+//         If the player chose "Rock", log "You win!";
+//         If the player chose "Paper", log "The computer wins!";
+//         If the player chose "Scissors", log "It's a tie!";
 function getResult(){
     if (compChoice === "rock"){
         if (playerChoice === "rock"){
@@ -45,21 +58,23 @@ function getResult(){
 let result = getResult();
 
 console.log(result);
-//     If the computer chose "Rock", check:
-//         If the player chose "Rock", log "It's a tie!";
-//         If the player chose "Paper", log "You win!";
-//         If the player chose "Scissors", log "The computer wins!";
-//     If the computer chose "Paper", check:
-//         If the player chose "Rock", log "The computer wins!";
-//         If the player chose "Paper", log "It's a tie!";
-//         If the player chose "Scissors", log "You win!";
-//     If the computer chose "Scissors", check:
-//         If the player chose "Rock", log "You win!";
-//         If the player chose "Paper", log "The computer wins!";
-//         If the player chose "Scissors", log "It's a tie!";
+
+// I'm going to try and set a variable for compScore and playerScore;
+// if the result is "you win", add one to playerScore;
+// if the result is "the computer wins", add one to compScore;
+// if the result is "tie", add one to tie maybe? Or do nothing
+
+
+
 // If the player wins, add a point to the player count;
 // If the computer wins, add a point to the computer count;
 // If the result is a tie, add a point to the tie count;
 //     The game is over when a total of 5 wins are accumulated;
 // After the game ends, declare a total winner;
 //     Provide stats (i.e., computer won x, player won y);
+}
+playRound();
+playRound();
+playRound();
+playRound();
+playRound();
